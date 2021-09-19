1d361971173dab4d6f7f696db6950dfde88a75c1
Jihad Bunkheila <jihadzackaria@gmail.com>
2021-09-19 05:09:10 -0400

    Fixed post request

--- popup.js
+++ popup.js
@@ -1,98 +1,112 @@
-let generateLink = document.querySelector('#generate');
-let copyLinkBtn = document.querySelector('#copyLinkBtn')
-let toastWarning = document.querySelector('.toast-warning')
-let toastSuccess = document.querySelector('.toast-success')
-let toastCopied = document.querySelector('.toast-primary')
-let toastError = document.querySelector('.toast-error')
-let toastClose = document.querySelector('#close')
-let mainLabel = document.querySelector('#mainLabel')
-let url = "http://localhost:5000/urlCombiner"
+let generateLink = document.querySelector("#generate");
+let copyLinkBtn = document.querySelector("#copyLinkBtn");
+let toastWarning = document.querySelector(".toast-warning");
+let toastSuccess = document.querySelector(".toast-success");
+let toastCopied = document.querySelector(".toast-primary");
+let toastError = document.querySelector(".toast-error");
+let toastClose = document.querySelector("#close");
+let mainLabel = document.querySelector("#mainLabel");
+let url = "http://localhost:5000/urlCombiner";
 
-var shortURL = "dfbsdjkf bsdjbflblwebljfbjlf"
+console.log(copyLinkBtn);
+console.log(mainLabel);
 
-generateLink.addEventListener('click', () => {
-    console.log("start generating link")
-    closeToasts()
-    toastWarning.classList.remove('d-hide')
+var shortURL = "shortURL";
 
-    chrome.tabs.query({currentWindow: true, highlighted: true}, function (tabs) {
-        closeToasts()
-        toastWarning.classList.remove('d-hide')
+generateLink.addEventListener("click", () => {
+  console.log("start generating link");
+  closeToasts();
+  toastWarning.classList.remove("d-hide");
 
-        fetch(url, {
-            method: "POST",
-            body: JSON.stringify({
-                "fullUrls": tabs.map(x => x.url),
-            })
-        }).then(response => response.json())
-            .then(json => {
-                closeToasts()
-                console.log(json)
-                // shortURL = json
+  chrome.tabs.query(
+    { currentWindow: true, highlighted: true },
+    function (tabs) {
+      closeToasts();
+      toastWarning.classList.remove("d-hide");
 
-                mainLabel.innerHTML = shortURL
-                
-                toastSuccess.classList.remove('d-hide')
-                generateLink.classList.add('d-hide')
-                copyLinkBtn.classList.remove('d-hide');
-                return;
-            })
-            .catch(err => {
-                closeToasts();
-                toastError.classList.remove('d-hide');
-                return;
-            })
-    });  
-});
+      const fullUrls = [];
 
-copyLinkBtn.addEventListener('click', async () => {
-    copyToClipboard()
-});
+      for (let i = 0; i < tabs.length; i++) {
+        fullUrls.push(tabs[i].url);
+      }
 
-mainLabel.addEventListener('click', async () => {
-    if (!copyLinkBtn,classList.contains('d-hide')) {
-        copyToClipboard()
+      fetch(url, {
+        method: "POST",
+        body: new URLSearchParams({ fullUrls: [fullUrls] }),
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          closeToasts();
+          console.log(json);
+          shortURL = json
+
+          mainLabel.innerHTML = shortURL;
+
+          toastSuccess.classList.remove("d-hide");
+          generateLink.classList.add("d-hide");
+          copyLinkBtn.classList.remove("d-hide");
+          return;
+        })
+        .catch((err) => {
+          closeToasts();
+          toastError.classList.remove("d-hide");
+          return;
+        });
     }
+  );
 });
 
-toastClose.addEventListener('click', async () => {
-    closeToasts()
+copyLinkBtn.addEventListener("click", async () => {
+  copyToClipboard();
+});
+
+mainLabel.addEventListener("click", async () => {
+  if ((!copyLinkBtn, classList.contains("d-hide"))) {
+    copyToClipboard();
+  }
+});
+
+toastClose.addEventListener("click", async () => {
+  closeToasts();
 });
 
 function copyToClipboard() {
-    closeToasts()
-    var copyFrom = document.createElement("textarea");
-    copyFrom.value = shortURL;
-    document.body.appendChild(copyFrom);
-    copyFrom.select();
-    document.execCommand('copy');
-    document.body.removeChild(copyFrom)
-    toastCopied.classList.remove('d-hide')
-    setTimeout(() => {
-        toastCopied.classList.add('d-hide')
-    }, 2000)
+  closeToasts();
+  var copyFrom = document.createElement("textarea");
+  copyFrom.value = shortURL;
+  document.body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand("copy");
+  document.body.removeChild(copyFrom);
+  toastCopied.classList.remove("d-hide");
+  setTimeout(() => {
+    toastCopied.classList.add("d-hide");
+  }, 2000);
 }
 
 function closeToasts() {
-    if (!toastError.classList.contains('d-hide')) {
-        toastError.classList.add('d-hide')
-    }
-    if (!toastSuccess.classList.contains('d-hide')) {
-        toastSuccess.classList.add('d-hide')
-    }
-    if (!toastWarning.classList.contains('d-hide')) {
-        toastWarning.classList.add('d-hide')
-    }
-    if (!toastCopied.classList.contains('d-hide')) {
-        toastCopied.classList.add('d-hide')
-    }
+  if (!toastError.classList.contains("d-hide")) {
+    toastError.classList.add("d-hide");
+  }
+  if (!toastSuccess.classList.contains("d-hide")) {
+    toastSuccess.classList.add("d-hide");
+  }
+  if (!toastWarning.classList.contains("d-hide")) {
+    toastWarning.classList.add("d-hide");
+  }
+  if (!toastCopied.classList.contains("d-hide")) {
+    toastCopied.classList.add("d-hide");
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    chrome.tabs.query({currentWindow: true, highlighted: true}, function (tabs) {
-        console.log(tabs.length)
-        let numOfTabs = 1;
-        numOfTabs = tabs.length;
-        document.getElementById("numTabs"),innerHTML = numOfTabs;
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.tabs.query(
+    { currentWindow: true, highlighted: true },
+    function (tabs) {
+      console.log(tabs.length);
+      let numOfTabs = 1;
+      numOfTabs = tabs.length;
+      document.getElementById("numTabs"), (innerHTML = numOfTabs);
+    }
+  );
 });
