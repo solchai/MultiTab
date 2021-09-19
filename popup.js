47131f8fddc48f3f146eec0323921bdf85bec672
solchai <sc908765@gmail.com>
2021-09-19 03:45:17 -0400

    fixed

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 let generateLink = document.querySelector('#generate');
 console.log(generateLink)
-let copyLinkBtn = document.querySelector('#copyLinkBtn');
+let copyLinkBtn = document.querySelector('#copyLinkBtn')
 let toastWarning = document.querySelector('.toast-warning')
 let toastSuccess = document.querySelector('.toast-success')
 let toastCopied = document.querySelector('.toast-primary')
 let toastError = document.querySelector('.toast-error')
 let toastClose = document.querySelector('#close')
-let mainLabel = document.querySelector('#mainlabel')
+let mainLabel = document.querySelector('#mainLabel')
 let url = "http://localhost:5000/urlCombiner"
 
 console.log(copyLinkBtn)
@@ -20,29 +20,6 @@ generateLink.addEventListener('click', () => {
     toastWarning.classList.remove('d-hide')
 
     chrome.tabs.query({currentWindow: true, highlighted: true}, function (tabs) {
-        fetch(apiCall).then(function(res) {
-            closeToasts();
-            if (res.status !== 200) {
-                toastError.classList.remove('d-hide');
-                return;
-            }
-            res.json().then(function(data) {
-                // do stuff with data
-                shortURL = "http://localhost:5000/" + data
-
-                mainLabel.innerHTML = shortURL
-                
-                toastSuccess.classList.add('d-hide')
-
-                generateURL.classList.add('d-hide');
-                copyLinkBtn.classList.remove('d-hide');
-                return;
-            });
-        }).catch(function(err) {
-            closeToasts();
-            toastError.classList.remove('d-hide');
-            return;
-        });
         closeToasts()
         toastWarning.classList.remove('d-hide')
 
@@ -105,17 +82,17 @@ function closeToasts() {
     if (!toastWarning.classList.contains('d-hide')) {
         toastWarning.classList.add('d-hide')
     }
-    if (!toastCopied,classList.contains('d-hide')) {
+    if (!toastCopied.classList.contains('d-hide')) {
         toastCopied.classList.add('d-hide')
     }
 }
 
-let numOfTabs = 0;
-// document.getElementById("numTabs").innerHTML = numOfTabs;
+// let numOfTabs = 0;
+// // document.getElementById("numTabs").innerHTML = numOfTabs;
 
-chrome.browserAction.onClicked.addListener(
-function (tabs) {
-    chrome.tabs.query({currentWindow: true, highlighted: true}, function (tabs) {
-        numOfTabs = tabs.length;
-    });
-});
+// chrome.browserAction.onClicked.addListener(
+// function (tabs) {
+//     chrome.tabs.query({currentWindow: true, highlighted: true}, function (tabs) {
+//         numOfTabs = tabs.length;
+//     });
+// });
